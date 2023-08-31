78d529e3d984134906dc44b314b1737cae2c2baa
muhardiansyah <muhardiansyah97@gmail.com>
2023-08-31 23:17:46 +0700

    Update manifest

--- user_activity_log/__manifest__.py
+++ user_activity_log/__manifest__.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': "User Activity Log",
-
-    'summary': """
-        User Activity Log""",
-
-    'description': """
-        User Activity Log
-    """,
-
+    'summary': 'Enhance security and transparency by tracking user activities and IP addresses in Odoo.',
+    'description': 'The "User Activity Log" module enables comprehensive tracking of user actions and IP addresses in the Odoo system. Enhance security, transparency, and accountability.',
     'author': "Muhardiansyah",
-    'website': "https://www.yourcompany.com",
-    'category': 'Extra Tools',
+    'website': "https://github.com/muhardiansyah15/hardi_addons",
+    'category': 'Tools/Tools',
+    'license': 'AGPL-3',
     'version': '0.1',
     'depends': ['base'],
     'data': [
